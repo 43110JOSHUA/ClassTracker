61e00d27009da7be67337157ece640124b8cf2b9
JoshuaTang <tangja2@uci.edu>
2025-07-03 21:59:53 +1000

    db can be created

--- engine.py
+++ engine.py
@@ -1,2 +1,16 @@
-# This is where the program is executed
-# Testing 1, 2
+import sqlite3
+
+connection = sqlite3.connect('class2023.db')
+cursor = connection.cursor()
+
+# Create database
+create_command = """CREATE TABLE IF NOT EXISTS students(
+    student_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    out_of_state INTEGER NOT NULL,
+    went_to_university INTEGER NOT NULL,
+    university_name TEXT,
+    occupation TEXT NOT NULL 
+    )"""
+
+cursor.execute(create_command)
