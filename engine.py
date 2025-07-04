54d38970f52fb9ff6cbeddfa8c75b76cf718bb1e
JoshuaTang <tangja2@uci.edu>
2025-07-04 23:47:44 +1000

    Added interface module

--- engine.py
+++ engine.py
@@ -1,18 +1,28 @@
+# This module runs the main program
 import sqlite3
+from interface import *
 
-connection = sqlite3.connect('class2023.db')
+connection = sqlite3.connect('yourClassData.db')
 cursor = connection.cursor()
 
-# Create database
+# STEP 1: Create Database
 create_command = """CREATE TABLE IF NOT EXISTS students(
     student_id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     out_of_state INTEGER NOT NULL,
-    occupation TEXT NOT NULL 
+    occupation TEXT NOT NULL ,
     went_to_university INTEGER NOT NULL,
-    university_name TEXT,
+    university_name TEXT
     )"""
-
 cursor.execute(create_command)
 connection.commit()
+
+# STEP 2: Main Loop
+help()
+while True:
+    break
+
+
+# STEP 3: Close Connection
+connection.close()
 
---
+++ interface.py
@@ -0,0 +1,9 @@
+# This module contains interface functions
+
+help_print = """1 - Add another classmate to the database.
+2 - Remove a classmate from the database.
+3 - Calculate statistics.
+"""
+
+def help():
+    print(help_print)
