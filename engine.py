51ac8743299614f48df4fc7b16306df78ec76297
JoshuaTang <tangja2@uci.edu>
2025-07-05 10:43:53 +1000

    Added more skeleton

--- engine.py
+++ engine.py
@@ -2,27 +2,56 @@
 import sqlite3
 from interface import *
 
-connection = sqlite3.connect('yourClassData.db')
-cursor = connection.cursor()
+db_name = 'yourClassData.db'
 
-# STEP 1: Create Database
-create_command = """CREATE TABLE IF NOT EXISTS students(
-    student_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    out_of_state INTEGER NOT NULL,
-    occupation TEXT NOT NULL ,
-    went_to_university INTEGER NOT NULL,
-    university_name TEXT
-    )"""
-cursor.execute(create_command)
-connection.commit()
-
-# STEP 2: Main Loop
-help()
-while True:
-    break
+def add_row():
+    pass
 
 
-# STEP 3: Close Connection
-connection.close()
+def remove_row():
+    pass
+
+
+def calc_stats():
+    pass
+
+
+if __name__ == "__main__":
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+
+    # STEP 1: Create Database
+    create_command = """CREATE TABLE IF NOT EXISTS students(
+        student_id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        out_of_state INTEGER NOT NULL,
+        occupation TEXT NOT NULL ,
+        went_to_university INTEGER NOT NULL,
+        university_name TEXT
+        )"""
+    cursor.execute(create_command)
+    connection.commit()
+
+    # STEP 2: Main Loop
+    help()
+    while True:
+        user_in = input()
+        if user_in == '1':
+            pass
+
+        elif user_in == '2':
+            pass
+
+        elif user_in == '3':
+            pass
+
+        elif user_in == '4':
+            quit()
+            break
+
+        else: # invalid input
+            help()
+
+    # STEP 3: Close Connection
+    connection.close()
 
--- interface.py
+++ interface.py
@@ -1,9 +1,16 @@
-# This module contains interface functions
+# This module contains interface messages
 
-help_print = """1 - Add another classmate to the database.
+help_message = """1 - Add another classmate to the database.
 2 - Remove a classmate from the database.
 3 - Calculate statistics.
+4 - Quit program.
 """
 
+quit_message = "Closing program."
+
+
 def help():
-    print(help_print)
+    print(help_message)
+
+def quit():
+    print(quit_message)
